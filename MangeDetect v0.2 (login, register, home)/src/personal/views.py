2ef1ef24467e4f54b9e5ed434ceff696d08dd9af
rekodeko1 <97767337+rekodeko1@users.noreply.github.com>
2024-09-25 13:15:22 +1000

    MangeDetect v0.2 (login, register, home)
    Added login, register, and home forms and functionality.

---
+++ MangeDetect v0.2 (login, register, home)/src/personal/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from account.models import Account
+# Create your views here.
+
+def home_screen_view(request):
+    
+    context = {}
+
+    accounts = Account.objects.all()
+    context['accounts'] = accounts
+
+    return render(request, "personal/home.html", context)
+
+def upload_page_view(request):
+    context = {}
+    return render(request, 'personal/upload_page.html', context)
+
+def about(request):
+    return render(request, 'personal/about.html')
