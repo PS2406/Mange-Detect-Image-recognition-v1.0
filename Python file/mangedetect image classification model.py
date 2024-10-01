1d1ee9d08f444318d95135698b3548feaf699bfe
Eternity4Ei <20421045@student.westernsydney.edu.au>
2024-10-01 15:07:11 +1000

    Update mangedetect image classification model.py
    change epoch number

--- Python file/mangedetect image classification model.py
+++ Python file/mangedetect image classification model.py
@@ -56,7 +56,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
 # Training loop
-num_epochs = 10
+num_epochs = 7
 for epoch in range(num_epochs):
     for phase in ['train', 'val']:
         if phase == 'train':
